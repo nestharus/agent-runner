1b603cdff2a58066683a299610faa59c7170b0c7
nestharus <contact@nestharus.com>
2026-08-10 16:13:51 -0700

    fix(resume): emit clean terminal signal marker

--- src-tauri/src/terminal_outcome_adapter/disposition.rs
+++ src-tauri/src/terminal_outcome_adapter/disposition.rs
@@ -52,7 +52,10 @@ pub fn apply_terminal_signal_outcome(
         | TerminalSignalDisposition::InteractiveFail => {
             apply_typed_post_failure_forensics_terminal_failure_side_effects(signal, ctx);
         }
-        TerminalSignalDisposition::InteractiveClean | TerminalSignalDisposition::NotApplicable => {}
+        TerminalSignalDisposition::InteractiveClean => {
+            emit_terminal_signal_marker_or_warn(signal, ctx)
+        }
+        TerminalSignalDisposition::NotApplicable => {}
     }
     disposition
 }
